a63c334f080677ff9653cdccf609d7e30ae2ce94
Shanattah <Magnus.taellgaard@hotmail.com>
2023-03-13 11:18:06 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import TodoApp from "./components/TodoApp";
 
-function App() {
-  const [count, setCount] = useState(0);
+const todos = [
+  { id: 1, task: "Syssla 1" },
+  { id: 2, task: "Syssla 2" },
+  { id: 3, task: "Syssla 3" },
+  { id: 4, task: "Syssla 4" },
+  { id: 5, task: "Syssla 5" },
+];
 
+function App() {
   return (
-    <div className="Todo">
-      <TodoApp />
+    <div>
+      <TodoApp todos={todos} />
     </div>
   );
 }
