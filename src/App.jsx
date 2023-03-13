c1633a6b30f965bc08f636d7f6f12d7e6df1ff88
liza <113092354+11liza@users.noreply.github.com>
2023-03-13 10:28:43 +0100

    .

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import TodoApp from './components/TodoApp'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="Todo">
+      <TodoApp />
+
+
+    </div>
+  )
+}
+
+export default App
