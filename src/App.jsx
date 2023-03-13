bd91b084120cb4afac853ee8b0b32a9aa6c6fd74
Shanattah <Magnus.taellgaard@hotmail.com>
2023-03-13 11:08:05 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import TodoApp from './components/TodoApp'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import TodoApp from "./components/TodoApp";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="Todo">
       <TodoApp />
-
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 /**
  * Part 1: Generate list (See Lists and keys lecture)
  * - Create a function component here, may call it 'TodoApp'
- * - generats a list <ul> from the array below, with the array-method .map()
+ * - generates a list <ul> from the array below, with the array-method .map()
  * - Place the code for listItems <li>, into a new child (function) component, may call it 'Todo'
  * - Add a key to every listItem <li>
  *
@@ -12,53 +12,45 @@ import React from 'react';
  * - The function will add a class .done, to the target element (e.target)
  * - Pass down the method as a prop, to the child component 'Todo'
  * - Make sure every listItem has the onClick eventhandler, that calls the method 'handleClick'
- * 
+ *
  * Part 3 (hard): Conditional rendering (google how to use conditions with .map() )
  * - Make sure every other listItem has the className="odd"
- * 
- * 
+ *
+ *
  * NOTE! Dont forget to add 'TodoApp' to App.jsx
  */
-
-
-function TodoApp() {
-    const todos = [
-        { id: 1, task: 'Syssla 1' },
-        { id: 2, task: 'Syssla 2' },
-        { id: 3, task: 'Syssla 3' },
-        { id: 4, task: 'Syssla 4' },
-        { id: 5, task: 'Syssla 5' }
-    ];
-
+function Todo({ task, index, onClick }) {
+  const className = index % 2 === 0 ? "" : "odd";
+  return (
+    <li className={className} onClick={onClick}>
+      {task}
+    </li>
+  );
 }
 
-function Todo() {
-    const listItems = id.map
-        ((id) =>
-            <ListItem key={id.toString()}
-                value={id} />
-        );
-    return (
-        <ul>
-            {listItems}
-        </ul>
-    );
+function TodoApp(props) {
+  function handleClick(e) {
+    e.target.classList.add("done");
+  }
 
+  const todos = props.todos.map((todo) => (
+    <Todo
+      key={todo.id}
+      task={todo.task}
+      index={todo.id - 1}
+      onClick={handleClick}
+    />
+  ));
 
+  return <ul>{todos}</ul>;
 }
 
+const todos = [
+  { id: 1, task: "Syssla 1" },
+  { id: 2, task: "Syssla 2" },
+  { id: 3, task: "Syssla 3" },
+  { id: 4, task: "Syssla 4" },
+  { id: 5, task: "Syssla 5" },
+];
 
-
-export default TodoApp
-
-
-
-
-
-
-
-
-
-
-
-
+export default TodoApp;
